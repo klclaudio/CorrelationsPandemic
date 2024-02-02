e85ea1bdc731a49ecef88fe3f8ca7758e4fd9bb7
klclaudio <klclaudio@gmail.com>
2024-02-02 12:23:12 -0300

    Conf options file  refactoring

--- conf-options.R
+++ conf-options.R
@@ -1,22 +1,10 @@
 #
 # Simulating options
 #
-# Erro  na simulação:
-#  1 - verificar montagem arquivos dataowd* pois possuem somente 200 dias de análise.
-#  2 - o arquivo base baixado do owdi está ok quanto aos dias.
-#  3 - verificar se houve mudanças nas colunas do arquivo base owdi *.csv .
-#    - as colunas estão ok
-#    - as linhas foram alteradas:
-#        os dados forma agrupados por semana nas novas versões dos arquivo da owdi.
-#  4- verificar montagem dos arquivos de dados levando em conta o o agrupamento semanal.
-#    - esse estavava ok
-#    Identificado o problema: o numero de semanas em análise estava sendo considerado errado, assim
-#    foram corrigidod os valores de N_i e Nx adequando-se ao número total de semanas.
-#
 
 data_site  <- 0 # 0 - use previously downloaded data, 1- download from owd site
-owid_file  <- "owid-covid-data19012024.csv"
-#owid_file <- "owid-covid-data19082020.csv"
+owid_file  <- "owid-covid-data19012024.csv" # downloaded data
+
 tdpm       <- 0 # 0 - cumulative deaths,  1- total deaths per million
 logdata    <- 1 # Data: 0- without log  transformation,  1- data with log transformation
 
